da6237df84b3b87611be68f60a62c2516fe9c394
baozhizhi850709@gmail.com <584614456bzz>
2018-04-04 07:23:01 +0800

    Initial commit

---
+++ src/main/java/com/douban/spider/common/CommonApplication.java
@@ -0,0 +1,14 @@
+package com.douban.spider.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonApplication.class, args);
+	}
+}
